3c3bb0ea3625fdbff039d347a09a002566fedfe5
sumitdevs <mysumitdevs@gmail.com>
2024-12-15 10:11:34 +0530

    public added

---
+++ public/scripts/script.js
@@ -0,0 +1,8 @@
+function copyToClipboard() {
+    const urlInput = document.getElementById('urlToCopy');
+    urlInput.select();
+    urlInput.setSelectionRange(0, 99999); // For mobile devices
+    navigator.clipboard.writeText(urlInput.value)
+        .then(() => alert('URL copied to clipboard!'))
+        .catch(err => console.error('Failed to copy URL:', err));
+}
